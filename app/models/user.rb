9705c0b3a839e3844c8a7032fbd9fbae43ae539e
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-02-12 01:25:38 +0100

    Users now have role (enum) First user in our database is the admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ActiveRecord::Base
+  enum role: [:user, :fs,:manager,:admin]
+  after_initialize :set_default_role, :if => :new_record?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def isAdmin
+    self.admin?
+  end
+  private
+##First User in the Database is an admin, rest defaults to user
+  def set_default_role
+    if User.count == 0
+      self.role ||= :admin
+    else
+      self.role ||= :user
+    end
+  end
 end
