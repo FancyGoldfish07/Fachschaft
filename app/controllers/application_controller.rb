366f488eb7779b363bca08328f4b0e607da908a2
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-02-18 04:43:03 +0100

    Errors raised by devise because we are not authorised now redirect.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #Rescue us from Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :permission_denied
+  rescue_from Pundit::NotDefinedError, with: :permission_denied
+  private
+  #redirects the bad boy to root or the refererer
+  def permission_denied
+    flash[:error] = "Da haben wir einen Riegel vorgeschoben und zwar keinen Schokoriegel!"
+
+    redirect_to(request.referrer || root_path)
+  end
 end
