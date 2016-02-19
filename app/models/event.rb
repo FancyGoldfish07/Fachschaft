b8a4a1588a398619c2ddd5b4b06259105fb8fbfa
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-02-19 12:30:35 +0100

    Event.priority is now an enum

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :roles
   belongs_to :event_category
+  after_initialize :set_default_priority, :if => :new_record?
+  #The priority
+  enum priority: [:highest, :high,:medium,:low,:lowest]
+  private
+  #Sets the default priority of the event
+  def set_default_priority
+self.priority ||= :medium
+  end
 end
