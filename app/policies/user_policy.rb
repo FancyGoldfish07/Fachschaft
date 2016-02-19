51bad55f48ed9130d44dbd10750c8969324dec17
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-02-19 05:19:56 +0100

    Policy update for rolify

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -5,12 +5,12 @@ class UserPolicy
     @user = model
   end
   def index?
-    @current_user.admin?
+    @current_user.isAdmin
   end
   def update?
-    @current_user.admin?
+    @current_user.isAdmin
   end
   def destroy?
-    @current_user == @user || @current_user.admin?
+    @current_user == @user || @current_user.isAdmin
   end
 end
