806741e17b62443fc690c23f3d0c12dd282632ee
Julian Handrup <julianhandrup@outlook.de>
2016-02-20 08:40:51 +0100

    'user_mailer' now supports html- and txt-email format

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,23 +1,43 @@
 class UserMailer < ApplicationMailer
 
   def signup_confirmation(user)
-    @user = user
+    mail(to: user.email) do |format|
+      format.html { render layout: '../user_mailer/role_change_confirmation.html.erb' }
+      format.text { render text: '../user_mailer/role_change_confirmation.html.erb' }
 
-    mail to: user.email, subject: "Fachschaft signup confirmation"
+      # @user = user
+
+      # mail to: user.email, subject: "Fachschaft signup confirmation"
+    end
   end
 
   def role_change_confirmation(user)
-    @user = user
-    mail to: user.email, subject: "User changed has been role"
+    mail(to: user.email) do |format|
+      format.html { render layout: '../user_mailer/role_change_confirmation.html.erb' }
+      format.text { render text: '../user_mailer/role_change_confirmation.html.erb' }
+
+      # @user = user
+      # mail to: user.email, subject: "User changed has been role"
+    end
   end
 
   def password_changed(user)
-    @user = user
-    mail to: user.email, subject: "Your Password has been updated"
+    mail(to: user.email) do |format|
+      format.html { render layout: '../user_mailer/password_changed' }
+      format.text { render text: '../user_mailer/password_changed' }
+
+      # @user = user
+      # mail to: user.email, subject: "Your Password has been updated"
+    end
   end
 
   def delete_account(user)
-    @user = user
-    mail to: user.email, subject: "Your Account has been deleted"
+    mail(to: user.email) do |format|
+      format.html { render layout: '../user_mailer/delete_account' }
+      format.text { render text: '../user_mailer/delete_account' }
+
+      # @user = user
+      # mail to: user.email, subject: "Your Account has been deleted"
+    end
   end
 end
