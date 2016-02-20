bbbf4516dca44a15916b8fc673d1784097739fea
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-02-20 14:25:14 +0100

    A Recurrence can now get its dates

--- app/models/recurrence.rb
+++ app/models/recurrence.rb
@@ -3,5 +3,20 @@ class Recurrence < ActiveRecord::Base
   include IceCube
   has_one :event
   has_many :rules
+#Get all appointments for our rules from start to end
+  def getDates(startDate, endDate)
+    schedule = IceCube::Schedule.new(startDate) do |s|
+      rules.each do |rule|
+      s.add_recurrence_rule(rule)
+      end
+      schedule.occurrences(endDate)
+    end
+
+  end
+  #Gets all dates from now for the next six months
+  def getDatesSixMonths(startDate)
+    getDates(startDate,startDate + 6.months)
+  end
+
 
 end
--- app/models/rule.rb
+++ app/models/rule.rb
@@ -3,19 +3,19 @@ class Rule < ActiveRecord::Base
   belongs_to :recurrence
   #Returns the days of the week in German
   def self.DAYS
-    ["Montag", "Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonntag"]
+    ["Sonntag","Montag", "Dienstag","Mittwoch","Donnerstag","Freitag","Samstag"]
   end
   #Returns the Ice_Cube rule
   def getRule
-    if month = 0
+    if month == 0
       IceCube::Rule.weekly(week).day(day)
     else
-      IceCube::Rule.monthly(month).day_of_week(day => days)
+      IceCube::Rule.monthly(month).day_of_week(day => [days])
       end
   end
   #Handles the default case where our week integer is 0 at startup
   def getWeek
-    if week = 0
+    if week == 0
       1
     else
      self.week
