e072657278db994a651b051d7e8541ef248336e1
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-02-21 01:32:47 +0100

    First wizard step is done kinda

--- app/controllers/events/build_controller.rb
+++ app/controllers/events/build_controller.rb
@@ -6,10 +6,20 @@ class Events::BuildController < ApplicationController
     render_wizard
   end
   def update
+    @event = Event.find(params[:event_id])
+    case step
+      when :build
+        @event.update(event_params)
+    end
 
+    render_wizard @event
   end
   def create
     @event = Event.create
     redirect_to wizard_path(steps.first,:event_id => @event.id)
+  end
+  private
+  def event_params
+    params.require(:event).permit(:title, :start, :priority, :flag, :imageURL, :url, :end, :ort, :description,role_ids: [])
   end
 end
