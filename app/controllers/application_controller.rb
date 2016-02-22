9ce1873205cd49c10b69dd775f5317843a8d3f1a
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-02-22 14:32:30 +0100

    Users now must have a username Be sure to drop your db!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   #Rescue us from Pundit
   rescue_from Pundit::NotAuthorizedError, with: :permission_denied
   rescue_from Pundit::NotDefinedError, with: :permission_denied
+
   private
   #redirects the bad boy to root or the refererer
   def permission_denied
@@ -15,4 +17,12 @@ class ApplicationController < ActionController::Base
 
     redirect_to(request.referrer || root_path)
   end
-end
+  #Allow us to have a username
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  end
+
+  end
+
