d910f8be72165f8f74b2eae151bb735169d77938
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-02-22 04:04:12 +0100

    Added some helpers for event.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,10 +3,13 @@ class Event < ActiveRecord::Base
   has_and_belongs_to_many :roles
   belongs_to :event_category
   belongs_to :recurrence
-  after_initialize :set_default_priority, :if => :new_record?
+  after_initialize :set_defaults, :if => :new_record?
   #The priority
   enum priority: [:highest, :high, :medium, :low, :lowest]
-
+#The duration of the event in seconds
+  def duration
+  return ((self.end - self.start)* 24 * 60 * 60)
+end
   #Set backgroundColor based on priority. This is used in the JSON
   def backgroundColor
     if self.highest?
@@ -18,8 +21,16 @@ class Event < ActiveRecord::Base
     end
     end
     private
-    #Sets the default priority of the event
-    def set_default_priority
+    #Sets the default priority of the event and start dates
+    def set_defaults
+      if self.priority.blank?
       self.priority ||= :medium
+      end
+      if self.start.blank?
+        start = DateTime.now
+      end
+      if self.end.blank?
+        self.end = start + 1.hour
+      end
     end
   end
