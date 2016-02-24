b3d716d3662dfd002215a7b5929e1b1ac07fc9a6
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-02-24 18:16:22 +0100

    Added a basic revision model to the database

--- app/models/Event_Version.rb
+++ app/models/Event_Version.rb
@@ -1,5 +1,11 @@
 #Event versioning for event model
 class EventVersion < PaperTrail::Version
+  belongs_to :author, class_name: 'User',
+             foreign_key: 'user_id'
+  belongs_to :admin,  class_name: 'User',
+                    foreign_key: 'admin_id'
+  belongs_to :manager, class_name: 'User',
+             foreign_key: 'manager_id'
   self.table_name = :event_versions
 
   self.sequence_name = :event_versions_id_seq
