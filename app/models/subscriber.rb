2f8569ca075f7c4b3483375559c0bd98b314260e
Jannik <jannikwortmann92@gmail.com>
2016-02-26 16:05:50 +0100

    Mailer cleanup

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,3 +1,14 @@
 class Subscriber < ActiveRecord::Base
 
+  after_save :send_signup_conf
+  before_destroy :destroy_confirmation
+
+  def send_signup_conf
+    NewsletterMailer.newsletter_signup_confirmation(@subscriber).deliver_later
+  end
+
+  def destroy_confirmation
+    NewsletterMailer.delete_subscription_conf(@subscriber).deliver
+  end
+
 end
