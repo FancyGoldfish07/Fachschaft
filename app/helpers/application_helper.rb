0f933f72ca8cbc72884713f9d87d80199df267ca
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-02-26 05:33:44 +0100

    Added title to each page Added link_to_delete helper behaves like button_to_delete helper sans styling Made the links in the navigation for the user sign in/sign up/sign out and change password work.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,14 @@ module ApplicationHelper
       link_to name, url, :method => :delete, class: "btn btn-w-md btn-gap-v btn-danger"
     end
   end
-
+  #Basically the same as button_to_delete, but without adding a class
+  def link_to_delete(name, url, toConfirm)
+    if toConfirm
+      link_to name, url, :method => :delete, data: { confirm: 'Bist du sicher?' }
+    else
+      link_to name, url, :method => :delete
+    end
+  end
   #Default button
   def button_to_default(name,url)
     link_to name, url, class: "btn btn-w-md btn-gap-v btn-default"
