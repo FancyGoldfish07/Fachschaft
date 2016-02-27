97f63b19a9b1b88c2882c4dc2fe4fb950b48b5fe
Florian Tiemann <Florian.Tiemann@fh-muenster.de>
2016-02-27 15:27:43 +0100

    Some more tests to check behaviour and also the models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,48 +1,78 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
-#   let(:user) {FactoryGirl.build(:user)}
-#   let(:fs) {FactoryGirl.build(:fs_user)}
-#   before(:each) do
-#     logout
-#   end
-#   fs.fs!
 
-  before :all do
-    @user = User.new
+  let(:user) {FactoryGirl.build(:user)}
+  let(:fs) {FactoryGirl.build(:fs_user)}
+
+
+
+  #Test whether important values are set
+
+  describe 'needs an email' do
+    it 'is valid with an email' do
+      expect(user).to be_valid
+    end
+    it 'is invalid without an email' do
+      user.email = nil
+      expect(user).not_to be_valid
+    end
   end
 
-  #Test whether important values are not set (nullable)
-
-  it 'is not valid to create an account without an email address' do
-    user = User.new encrypted_password: '12345678'
-
-    expect(user).to_not be_valid
+  describe 'needs a username' do
+    it 'is valid with an username' do
+      expect(user).to be_valid
+    end
+    it 'is invalid without an username' do
+      user.username = nil
+      expect(user).not_to be valid
+    end
   end
 
-  it 'is required to set a password with minimum 8 chars' do
-    user = User.new email: 'test@fachschaft.de', encrypted_password: '1234567'
-
-    expect(user).to_not be_valid
+  describe 'needs a password at minimum 8 chars' do
+    it 'is valid a with password which contains min 8 chars' do
+      expect(user).to be_valid
+    end
+    it 'is invalid without a password' do
+      user.password = nil
+      expect(user).not_to be be_valid
+    end
+    it 'is in valid with a shorten password' do
+      user.password = '1234567'
+      expect(user).not_to be_valid
+    end
   end
 
-  it 'is require that an email is unique' do
-    user1 = User.new email: 'test@fachschaft.de', encrypted_password: '12345678'
-    user2 = User.new email: 'test@fachschaft.de', encrypted_password: '23456789'
-
-    expect(user2).to_not be_valid
+  describe 'needs an unique email' do
+    it 'is invalid without a unique email' do
+      # user and fs_user both have the same email address
+      expect(fs).not_to be_valid
+    end
+    it 'is valid wih several email addresses' do
+      # user.email is info@fachschaft.de
+      fs.email = 'test@fachschaft.de'
+      expect(fs).to be_valid
+    end
   end
 
-  it 'is required that an username is unique' do
-    user1 = User.new email: 'test@fachschaft.de', encrypted_password: '12345678', username: 'RSPEC_Test'
-    user2 = User.new email: 'info@fachschaft.de', encrypted_password: '23456789', username: 'RSPEC_Test'
-
-    expect(user2).to_not be_valid
+  describe 'needs an unique username' do
+    it 'is invalid without an unique username' do
+      # user and fs both have username 'Test'
+      expect(fs).not_to be_valid
+    end
+    it 'is valid with several usernames' do
+      #user.username is 'Test'
+      fs.username = 'AnotherName'
+      expect(fs).to be_valid
+    end
   end
 
-  it 'is required not to set an email as username' do
-    user = User.new email: 'test@fachschaft.de', encrypted_password: '12345678', username: 'test@test.de'
-
-    expect(user).to_not be_valid
+  describe 'needs an username which is not an email address' do
+    it 'is valid with a username which is not an email address' do
+      expect(user).to be_valid
+    end
+    it 'is not valid with a username format like an email address' do
+      user.username = 'test@test.de'
+      expect(user).not_to be_valid
+    end
   end
-
-  end
+end
