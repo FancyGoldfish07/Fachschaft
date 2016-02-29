f30ee76982a527b7377cbcdf14262ac4832232b4
Julian Handrup <julianhandrup@outlook.de>
2016-02-29 03:11:09 +0100

    added new test-users to seed: admin/manager/user@example.com with userSparten123 etc. as password.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228183441) do
+ActiveRecord::Schema.define(version: 20160227150306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,16 +141,6 @@ ActiveRecord::Schema.define(version: 20160228183441) do
   end
 
   add_index "rules", ["recurrence_id"], name: "index_rules_on_recurrence_id", using: :btree
-
-  create_table "sessions", force: :cascade do |t|
-    t.string   "session_id", null: false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
-  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "subscribers", force: :cascade do |t|
     t.string   "name"
