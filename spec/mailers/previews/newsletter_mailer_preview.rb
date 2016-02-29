e10d186f86e61269f34003c16f8f1d58b800da6d
Florian Tiemann <Florian.Tiemann@fh-muenster.de>
2016-02-29 20:58:48 +0100

    Fixed RSpec test problems

--- spec/mailers/previews/newsletter_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/newsletter_mailer
-class NewsletterMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/newsletter_mailer/send_newsletter
-  def send_newsletter
-    NewsletterMailer.send_newsletter
-  end
-
-end
--- spec/mailers/previews/notification_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/notification_mailer
-class NotificationMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/notify
-  def notify
-    NotificationMailer.notify
-  end
-
-end
