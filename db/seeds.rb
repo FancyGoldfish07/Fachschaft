d62a77ca496f9a50041c002caf3d1fa461892317
Julian Handrup <julianhandrup@outlook.de>
2016-02-29 09:55:04 +0100

    edited seeds.rb to firstly create the users. New standard pw: Sparten123

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,24 +12,42 @@ Role.create([{ name: 'User' }, { name: 'Fachschaft' },{name: 'Manager'},{name:"A
 # Create event categories on startup.
 EventCategory.create([{name: 'Fachschaftssitzung'}, {name: 'Party'}])
 
+
+# Create test-users for Heroku
+admin = User.create(email:"admin@example.com", password: "Sparten123", username:"testadmin")
+admin.save
+
+manager = User.create(email:"manager@example.com", password: "Sparten123", username:"testmanager", )
+manager.save
+manager.setRole("Manager")
+
+
+fachschaft = User.create(email:"fachschaft@example.com", password: "Sparten123", username:"testfachschaft", )
+fachschaft.save
+fachschaft.setRole("Fachschaft")
+
+user = User.create(email: "user@example.com", password:"UserSparten123", username:"testuser")
+user.save
+# automatically sets role to user.
+
+
 # Create sample events on startup (only needed for the deployment on Heroku and testing purposes)
 event1 = Event.create(start: Time.now + 1.day,title: "Testtermin",event_category: EventCategory.first)
 event1.save
+# Change state of event1 to waiting for review
+event1.changeState(fachschaft)
+
+
 event2 = Event.create(start: Time.now + 2.day,title: "Party Party Paty",event_category: EventCategory.first)
 event2.save
+# Change state of event2 to waiting and then to reviewed meaning waiting to be published
+event2.changeState(fachschaft)
+event2.changeState(admin)
+
 event3 = Event.create(start:Time.now + 1.hour, end: Time.now+3.hour,title: "Example", event_category: EventCategory.last,
                       imageURL:"https://i.ytimg.com/vi/icIeIJhpxLc/maxresdefault.jpg", url:"http://winfuture.de/downloadvorschalt,3527.html",
                       description: "Party Hard")
 event3.save
+event3.publish
 
-# Create test-users for Heroku
-user1 = User.create(email:"admin@example.com", password: "AdminSparten123", username:"testadmin")
-user1.save
 
-user2 = User.create(email:"manager@example.com", password: "ManagerSparten123", username:"testmanager", )
-user2.save
-user2.setRole("Manager")
-
-user3 = User.create(email: "user@example.com", password:"UserSparten123", username:"testuser")
-user3.save
-# automatically sets role to user.
