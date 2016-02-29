d03412bdda93aa87baa3b4479e787c0b596727bc
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-02-29 17:09:43 +0100

    Password change spec updated

--- spec/features/change_password_spec.rb
+++ spec/features/change_password_spec.rb
@@ -13,12 +13,12 @@ RSpec.feature 'As a registered user I want to change my password' do
       within ('#myNavbar') do
         click_link ('Passwort ändern')
       end
-      fill_in 'user_password', :with => '12345678'
-      fill_in 'user_password_confirmation', :with => '12345678'
+      fill_in 'user_password', :with => 'Sparten123'
+      fill_in 'user_password_confirmation', :with => 'Sparten123'
       fill_in 'user_current_password', :with => '12345678'
 
-      click_button 'Abschicken'
-      expect(page).to have_content 'Erfolgreich geändert'
+      click_button 'Update'
+      expect(page).to have_content 'Deine Daten wurden aktualisiert.'
     end
   end
 end
